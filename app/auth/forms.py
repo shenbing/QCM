297efe5ea23da775463e96849f71973b5fbca528
shenbing <shenbing19880101@163.com>
2018-06-25 08:49:07 +0800

    修改登录页，错误信息提示

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -9,6 +9,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,validators,PasswordField,SelectField,BooleanField
 
 class LoginFrom(FlaskForm):
+    class Meta:
+        csrf = False
     username=StringField(u'用户名',[validators.Length(min=4, max=16,message=u'用户名长度在4-16位'),validators.DataRequired()],render_kw={'placeholder':u'请输入用户名'})
     password=PasswordField(u'密码',[validators.Length(min=6,max=16,message=u'密码长度6-16位'),validators.DataRequired()],render_kw={'placeholder':u'请输入密码'})
     rememberme = BooleanField('保持登录')
--- app/auth/views.py
+++ app/auth/views.py
@@ -5,7 +5,7 @@
 @time: 2018/6/8 14:30
 """
 
-from flask import render_template, request, jsonify, session, url_for, redirect
+from flask import render_template, request, jsonify, session, url_for, redirect, flash
 from flask.views import MethodView, View
 from app.auth.forms import LoginFrom
 from flask_login import login_user
@@ -17,23 +17,19 @@ class LoginView(MethodView):
 
     def get(self):
         form = LoginFrom()
-        return render_template("auth/login.html", form=form)
+        return render_template("auth/login.html")
 
     def post(self):
-        form = LoginFrom(csrf_enabled=False)
-        if form.validate_on_submit():
-            data = form.data
-            username = data['username']
-            password = data['password']
-            if username is None:
-                return jsonify({'msg': u'用户名没有输入', 'code': 33, 'data': ''})
-            if password is None:
-                return jsonify({'msg': u'密码没有输入', 'code': 34, 'data': ''})
-            user = User.query.filter_by(user_name=func.binary(username)).first()
-            if user:
-                if user.verify_password(password):
-                    login_user(user, data['rememberme'])
-                    session['username'] = username
-                    return redirect(url_for("home.index"))
-                return jsonify({'msg': u'密码错误', 'code': 36, 'data': ''})
-            return jsonify({'msg': u'用户不存在', 'code': 37, 'data': ''})
+        form = LoginFrom()
+        username = form.data['username']
+        password = form.data['password']
+        user = User.query.filter_by(user_name=func.binary(username)).first()
+        if user:
+            if user.verify_password(password):
+                login_user(user, form.data['rememberme'])
+                session['username'] = username
+                return redirect(url_for("home.index"))
+            flash("用户密码不正确")
+            return render_template("auth/login.html")
+        flash("用户不存在")
+        return render_template("auth/login.html")
